25cdef652bd0642249dd1c6218bedffc4e32b7a7
tash1979 <tash1979@rambler.ru>
2019-11-24 00:48:12 +0300

    hw

---
+++ models/Activity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergeytashkinov
+ * Date: 2019-11-24
+ * Time: 00:42
+ */
+
+namespace app\models;
+
+
+class Activity
+{
+
+}
---
+++ models/Day.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergeytashkinov
+ * Date: 2019-11-24
+ * Time: 00:45
+ */
+
+namespace app\models;
+
+
+class Day
+{
+
+}
